57815b3ae9cf1be2f8e93e6aeaf5b08eba2a9ac9
Joao Pedro Portella Guedes Visconti <JOVSI@vestas.com>
2023-10-31 08:02:13 +0000

    Create mutation to register an User. Missign the validation

--- server/resolvers/users.js
+++ server/resolvers/users.js
@@ -15,7 +15,35 @@ export default {
       // TODO: Validate user data
       // TODO: Make sure that user doesn't exist
       // TODO: Make sure that username is unique
-      // TODO: Hash password and create an auth token
+
+      // Hash the password before saving
+      password = await bcrypt.hash(password, 12)
+
+      const newUser = new User({
+        email,
+        username,
+        password,
+        createdAt: new Date().toISOString(),
+      })
+
+      const res = await newUser.save()
+
+      // Create an auth token
+      const token = jwt.sign(
+        {
+          id: res.id,
+          email: res.email,
+          username: res.username,
+        },
+        SECRET_KEY,
+        { expiresIn: '1h' }
+      )
+
+      return {
+        ...res._doc,
+        id: res._id,
+        token,
+      }
     },
   },
 }
