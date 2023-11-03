15bd86294121f76260a03e29cd32c1770503e190
Joao Pedro Portella Guedes Visconti <JOVSI@vestas.com>
2023-11-03 07:00:23 +0000

    Checking if username and email are already registered

--- server/resolvers/users.js
+++ server/resolvers/users.js
@@ -1,5 +1,6 @@
 import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
+import { UserInputError } from 'apollo-server'
 
 import { SECRET_KEY } from '../config.js'
 import User from '../models/User.js'
@@ -15,6 +16,22 @@ export default {
       // TODO: Validate user data
       // TODO: Make sure that user doesn't exist
       // TODO: Make sure that username is unique
+      const userName = await User.findOne({ username })
+      if (userName) {
+        throw new UserInputError('Username is taken', {
+          errors: {
+            username: 'This username is taken',
+          },
+        })
+      }
+      const userEmail = await User.findOne({ email })
+      if (userEmail) {
+        throw new UserInputError('Email already exist', {
+          errors: {
+            email: 'Email already exist',
+          },
+        })
+      }
 
       // Hash the password before saving
       password = await bcrypt.hash(password, 12)
