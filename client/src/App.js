3c1ca2cc0412b67e4aea3edcde5eed3ed2cb36f2
unknown <visconti.joao@gmail.com>
2023-11-05 11:12:58 +0000

    Start to add routes to client

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,29 @@
 import './App.css'
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './pages/Home'
+import Register from './pages/Register'
+import Login from './pages/Login'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/register',
+    element: <Register />,
+  },
+])
+
 function App() {
   return (
     <div className='App'>
-      <h1>Hi World</h1>
+      <RouterProvider router={router} />
     </div>
   )
 }
