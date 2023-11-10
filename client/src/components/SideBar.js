3b84a4963905cb6b1bae08bb5ed0c742bdfd0598
Joao Pedro Portella Guedes Visconti <JOVSI@vestas.com>
2023-11-10 15:23:13 +0000

    Trying to create the SideBar, it is not showing on page

--- client/src/components/SideBar.js
+++ client/src/components/SideBar.js
@@ -13,39 +13,19 @@ export default function SideBar() {
   const [visible, setVisible] = useState(false)
 
   return (
-    <Grid columns={1}>
-      <Grid.Column>
-        <Checkbox
-          checked={visible}
-          label={{ children: <code>visible</code> }}
-          onChange={(e, data) => setVisible(data.checked)}
-        />
-      </Grid.Column>
-      <Grid.Column>
-        <Sidebar.Pushable as={Segment}>
-          <SideBar
-            as={Menu}
-            animation="overlay"
-            icon="labeled"
-            inverted
-            onHide={() => setVisible(false)}
-            vertical
-            visible={visible}
-            width="thin"
-          >
-            <Menu.Item as="a">
-              <Icon name="home" />
-              Home
-            </Menu.Item>
-          </SideBar>
-
-          <Sidebar.Pusher dimmed={visible}>
-            <Segment basic>
-              <Header as="h3">Application Content</Header>
-            </Segment>
-          </Sidebar.Pusher>
-        </Sidebar.Pushable>
-      </Grid.Column>
-    </Grid>
+    <Sidebar as="Menu">
+      <Menu.Item as="a">
+        <Icon name="home" />
+        Home
+      </Menu.Item>
+      <Menu.Item as="a">
+        <Icon name="gamepad" />
+        Games
+      </Menu.Item>
+      <Menu.Item as="a">
+        <Icon name="camera" />
+        Channels
+      </Menu.Item>
+    </Sidebar>
   )
 }
